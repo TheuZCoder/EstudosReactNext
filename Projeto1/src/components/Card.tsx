a658a541016ad98ebe98390128a6dc62832ea4e7
TheuZCoder <matheusrdsilva1@gmail.com>
2024-03-26 09:35:58 -0300

    create: aprendido criação da renderização condicional

--- Projeto1/src/components/Card.tsx
+++ Projeto1/src/components/Card.tsx
@@ -7,12 +7,17 @@ interface CardProps{
 
 
 export default function Card(props: CardProps) {
-    return(
-        <div className="flex flex-col border border-red-700 rounded-sm p-1">
+    return(                                 //aqui temos uma função de renderização que necessita de uma segunda opção para funcionar
+        <div className={`flex flex-col border-4 ${props.desconto>0?'border-red-700':'border-blue-700'}rounded-sm p-1`}>
             <div>Produto:{props.produto}</div>
             <div>Valor: R${props.valor}</div>
-            <div>Desconto: R${props.desconto}</div>
-            <div>Preço venda: R${ props.funcao (props.valor,props.desconto)}</div>
-        </div>
+
+            {props.desconto>0&&( //aqui vemos uma função de renderização condicional que só aparece caso o desconto for maior que 0 e não tem uma segunda função
+                <div>
+                    <div>Desconto: R${props.desconto}</div>
+                    <div>Preço venda: R${ props.funcao (props.valor,props.desconto)}</div> 
+                </div>
+            )}
+        </div>// sempre utilizar props porque é oq esta sendo chamado na função CARD(props: CardProps)
     )
 }
